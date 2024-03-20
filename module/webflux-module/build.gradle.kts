420dc6064ed9afa6e86278ae4bfd0cbc402e1339
seminchoi <choicco89@gmail.com>
2024-03-20 18:48:20 +0900

    feat: webflux-module 의존성 추가

--- module/webflux-module/build.gradle.kts
+++ module/webflux-module/build.gradle.kts
@@ -3,11 +3,11 @@ tasks.bootJar { enabled = false }
 tasks.jar { enabled = true }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    api("org.springframework.boot:spring-boot-starter-webflux")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    api("io.projectreactor.kotlin:reactor-kotlin-extensions")
 
-    implementation ("io.netty:netty-resolver-dns-native-macos:4.1.68.Final:osx-aarch_64")
+    api ("io.netty:netty-resolver-dns-native-macos:4.1.68.Final:osx-aarch_64")
 
-    testImplementation("io.projectreactor:reactor-test")
+    testApi("io.projectreactor:reactor-test")
 }
